b86ac6c9d4c77338ea59a4dd07abf399d446b88a
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2024-01-30 23:59:17 +0500

    setCapital added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core'
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 
+interface CityMap {
+  [key: string]: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnChanges{
+export class AppComponent implements OnInit, OnChanges {
   form: FormGroup = new FormGroup({
     email: new FormControl(
       '',
       [
-      Validators.required,
-      Validators.email,
-    ]),
+        Validators.required,
+        Validators.email,
+      ]),
     password: new FormControl(
       null,
       [
+        Validators.required,
         Validators.minLength(4),
-        Validators.minLength(4)
-      ]
-      )
+      ]),
+    address: new FormGroup({
+      country: new FormControl('ua'),
+      city: new FormControl('', Validators.required)
+    })
   })
 
   ngOnInit() {
@@ -31,9 +38,23 @@ export class AppComponent implements OnInit, OnChanges{
   }
 
   submit() {
-    console.log(this.form.get('password'));
+    console.log(this.form.value);
   }
 
   protected readonly toolbar = toolbar;
+
+  cityMap: CityMap = {
+    ru: 'Москва',
+    ua: 'Киев',
+    by: 'Минск',
+  }
+
+  setCapital() {
+    const country = this.form.get('address')?.get('country')?.value
+
+    this.form.patchValue({
+      address: {city: this.cityMap[country]}
+    })
+  }
 }
 
